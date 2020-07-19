d559011b417e5d4160104d91ce321956308170a1
Kareem Hunte <kareemhunte@gmail.com>
2020-07-19 11:22:57 -0400

    Revert "Setup api framework"

--- bilbal-backend/server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const sample = require('./sample');
-
-function setRoutes(app, config, logger) {
-    app.use('/sample', sample.sample(config, logger));
-}
-
-module.exports = {
-    setRoutes: setRoutes
-};
